c000c11b9eb745197a107e6eaea9d028972c428c
khemmathat bootchim <s7070012@kmitl.ac.th>
2018-09-18 21:18:01 +0700

    add pop up in file main.js & index.xhtml

--- shinysdr/i/webstatic/client/main.js
+++ shinysdr/i/webstatic/client/main.js
@@ -239,6 +239,23 @@ define([
           resize.initEvent('resize', false, false);
           window.dispatchEvent(resize);
         }, 100);
+
+        // Start edit here
+        var modal = document.getElementById('myModal');
+        var span = document.getElementsByClassName("close")[0];
+        var myBtnDFScan = document.getElementById("myBtnDFScan")
+        myBtnDFScan.onclick = function() {
+          modal.style.display = "block";
+        }
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+        // End here
         
         // globals for debugging / interactive programming purposes only
         window.DfreqDB = freqDB;
